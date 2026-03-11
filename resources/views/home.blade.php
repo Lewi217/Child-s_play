6671c84b801b084e81c1b5c48d98925c00ce5f80
wanjohi lewi <wanjohilewi7@gmail.com>
2026-03-11 10:32:57 +0300

    init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style = "border: 3px solid black;">
+       <h1>Register</h1>
+       <form action="/register" method="POST">
+        @csrf
+        <input type="text" name="name" id="name">
+        <input type="email" name="email" id="email">
+        <input type="password" name="password" id="password">
+        <button type="submit">Register</button>
+       </form>
+    </div>
+</body>
+</html>
