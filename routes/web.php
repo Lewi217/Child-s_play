0856ad5c5f89599b3a032d04e14fe3b82760754f
wanjohi lewi <wanjohilewi7@gmail.com>
2026-03-23 12:10:50 +0300

    display of posts

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Post::all();
-    return view('home');
+    $posts = Post::where('user_id', auth()->id())->get();
+    return view('home', ['posts' => $posts]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
