3aae74e9c413a532a059dcce3833adeaee80ed9e
wanjohi lewi <wanjohilewi7@gmail.com>
2026-03-27 00:31:23 +0300

    edit & delete posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,7 @@ class Post extends Model
 
     protected $fillable = ['title', 'body', 'user_id'];
     
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
